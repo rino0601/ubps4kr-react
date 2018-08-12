9189871bef6bf7f5767155e18701c208bb40361b
rino0601 <rino0601.dev@gmail.com>
2018-08-13 01:26:44 +0900

    start react-native

--- src/component/SmartFAB.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by rino0 on 2017-02-06.
- */
--- src/redux/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by rino0 on 2017-02-06.
- */
-import {combineReducers} from "redux";
-
-export default combineReducers({
-
-});
